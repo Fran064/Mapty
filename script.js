d62e7751388655a162ff37fa0ed1e4685dfc5643
Braeden Francisco <91705227+Fran064@users.noreply.github.com>
2024-02-05 12:45:43 -0600

    Update script.js

--- script.js
+++ script.js
@@ -6,15 +6,12 @@ class Workout {
   clicks = 0;
 
   constructor(coords, distance, duration) {
-    // this.date = ...
-    // this.id = ...
     this.coords = coords; // [lat, lng]
     this.distance = distance; // in km
     this.duration = duration; // in min
   }
 
   _setDescription() {
-    // prettier-ignore
     const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
     this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${
@@ -56,18 +53,11 @@ class Cycling extends Workout {
   }
 
   calcSpeed() {
-    // km/h
     this.speed = this.distance / (this.duration / 60);
     return this.speed;
   }
 }
 
-// const run1 = new Running([39, -12], 5.2, 24, 178);
-// const cycling1 = new Cycling([39, -12], 27, 95, 523);
-// console.log(run1, cycling1);
-
-///////////////////////////////////////
-// APPLICATION ARCHITECTURE
 const form = document.querySelector('.form');
 const containerWorkouts = document.querySelector('.workouts');
 const inputType = document.querySelector('.form__input--type');
@@ -83,13 +73,10 @@ class App {
   #workouts = [];
 
   constructor() {
-    // Get user's position
     this._getPosition();
 
-    // Get data from local storage
     this._getLocalStorage();
 
-    // Attach event handlers
     form.addEventListener('submit', this._newWorkout.bind(this));
     inputType.addEventListener('change', this._toggleElevationField);
     containerWorkouts.addEventListener('click', this._moveToPopup.bind(this));
@@ -108,7 +95,6 @@ class App {
   _loadMap(position) {
     const { latitude } = position.coords;
     const { longitude } = position.coords;
-    // console.log(`https://www.google.pt/maps/@${latitude},${longitude}`);
 
     const coords = [latitude, longitude];
 
@@ -119,7 +105,6 @@ class App {
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(this.#map);
 
-    // Handling clicks on map
     this.#map.on('click', this._showForm.bind(this));
 
     this.#workouts.forEach(work => {
@@ -134,7 +119,6 @@ class App {
   }
 
   _hideForm() {
-    // Empty inputs
     inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value =
       '';
 
@@ -155,22 +139,19 @@ class App {
 
     e.preventDefault();
 
-    // Get data from form
+
     const type = inputType.value;
     const distance = +inputDistance.value;
     const duration = +inputDuration.value;
     const { lat, lng } = this.#mapEvent.latlng;
     let workout;
 
-    // If workout running, create running object
     if (type === 'running') {
       const cadence = +inputCadence.value;
 
-      // Check if data is valid
+     
       if (
-        // !Number.isFinite(distance) ||
-        // !Number.isFinite(duration) ||
-        // !Number.isFinite(cadence)
+   
         !validInputs(distance, duration, cadence) ||
         !allPositive(distance, duration, cadence)
       )
@@ -179,7 +160,7 @@ class App {
       workout = new Running([lat, lng], distance, duration, cadence);
     }
 
-    // If workout cycling, create cycling object
+  
     if (type === 'cycling') {
       const elevation = +inputElevation.value;
 
@@ -192,19 +173,16 @@ class App {
       workout = new Cycling([lat, lng], distance, duration, elevation);
     }
 
-    // Add new object to workout array
+
     this.#workouts.push(workout);
 
-    // Render workout on map as marker
     this._renderWorkoutMarker(workout);
 
-    // Render workout on list
+
     this._renderWorkout(workout);
 
-    // Hide form + clear input fields
     this._hideForm();
 
-    // Set local storage to all workouts
     this._setLocalStorage();
   }
 
@@ -278,7 +256,7 @@ class App {
   }
 
   _moveToPopup(e) {
-    // BUGFIX: When we click on a workout before the map has loaded, we get an error. But there is an easy fix:
+   
     if (!this.#map) return;
 
     const workoutEl = e.target.closest('.workout');
@@ -296,8 +274,7 @@ class App {
       },
     });
 
-    // using the public interface
-    // workout.click();
+  
   }
 
   _setLocalStorage() {
